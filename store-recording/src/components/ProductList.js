6d8b59854204a0247687eff5d142ffd969503b73
aminedammak <amine.dammak@softtodo.com>
2019-10-17 16:49:14 +0100

    Created basic components and implement routing

---
+++ store-recording/src/components/ProductList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Product from './Product';
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                Hello from product list
+            </div>
+        );
+    }
+}
+
+export default ProductList;
